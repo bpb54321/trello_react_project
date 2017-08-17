c8be750c6e5461fc4adcd0566988d5b14ce20915
Brian Blosser <bpb54321@gmail.com>
2017-08-17 18:49:10 -0400

    Added a Card component with some markup, and added three cards into the CardColumn render() method.

--- src/components.jsx
+++ src/components.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 
 /* Define Card Component Here*/
+class Card extends React.Component {
+  render() {
+    return (
+      <li className="card">
+        <div className="title">Take React Course</div>
+        <div className="description">code on code on code on code...</div>
+        <div className="actions">
+          <button className="delete-card">X</button>
+          <button className="complete-card">></button>
+        </div>
+      </li>
+    );
+  }
+}
 
 /* Column Component */
 class CardColumn extends React.Component {
+
+    constructor() {
+      super();
+
+      this.state = {
+        cards: [
+          {
+            title: 'Cake Homepage HTML/CSS',
+            description: 'Code on Code on Code on Code',
+            actions: 'blah'
+          }
+        ]
+      };
+    }
     render() {
-        return (<div></div>);
+        return (
+          <div className="card-column">
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+        );
     }
 }
 
 export default CardColumn;
+
+// I want each Card Column to display an array of cards.
+// To do this, I should create a state array in CardColumn which contains an array of Card objects.
