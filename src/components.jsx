b4644e869efc81aff8cc1dcf3521a6fce81fa0b0
Brian Blosser <bpb54321@gmail.com>
2017-08-19 10:23:29 -0400

    Put all the components in components.jsx.

--- src/components.jsx
+++ src/components.jsx
@@ -1,7 +1,99 @@
 import React from 'react';
 
+export class Page extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <Sidebar/>
+        <Main/>
+      </div>
+    );
+  }
+}
+
+export class Sidebar extends React.Component {
+  render() {
+    return (
+      <div id="sidebar">
+        <div id="add-card-form">
+          <div className="form-title">New Card</div>
+          <input id="title-text" type="text" />
+          <input id="description-text" type="text" />
+          <button className="add-card">+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      columns: [
+        {
+          cards: [
+            {
+              title: 'Cake Homepage HTML/CSS',
+              description: 'Code on Code on Code on Code',
+              hasDeleteAction: true,
+              hasCompleteAction: true,
+              id: 1,
+            },
+            {
+              title: 'Learn React',
+              description: 'Take the first Level of React',
+              hasDeleteAction: true,
+              hasCompleteAction: true,
+              id: 2,
+            },
+            {
+              title: 'Plan Two Truths and a Lie',
+              description: 'Finish Wireframe, start HTML Layout with some other things...',
+              hasDeleteAction: true,
+              hasCompleteAction: true,
+              id: 3,
+            },
+          ],
+        },
+        {
+          cards: [
+            {
+              title: 'Cake Homepage HTML/CSS',
+              description: 'Code on Code on Code on Code',
+              hasDeleteAction: true,
+              hasCompleteAction: true,
+              id: 1,
+            },
+          ],
+        },
+      ],
+    };
+
+  }
+
+  render () {
+    return (
+      <div id="main">
+        <CardColumn cards={this.state.columns[0].cards} />
+        <CardColumn cards={this.state.columns[1].cards} />
+      </div>
+    );
+  }
+}
+
+export class CardForm extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
 /* Define Card Component Here*/
-class Card extends React.Component {
+export class Card extends React.Component {
   render() {
 
     let deleteButton = '';
@@ -29,7 +121,7 @@ class Card extends React.Component {
 }
 
 /* Column Component */
-class CardColumn extends React.Component {
+export class CardColumn extends React.Component {
 
     constructor() {
       super();
@@ -56,8 +148,6 @@ class CardColumn extends React.Component {
         );
     }
 }
-
-export default CardColumn;
 
 // I want each Card Column to display an array of cards.
 // To do this, I should create a state array in CardColumn which contains an array of Card objects.
